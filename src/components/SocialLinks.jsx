9c74273ffc26f2d1729c9a19be055e47a13ac9a5
1Njeru <122145897+1Njeru@users.noreply.github.com>
2023-06-12 14:57:10 +0300

    Update SocialLinks.jsx

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -9,7 +9,7 @@ const SocialLinks = () => {
 
 
   return (
-    <div className='hidden lg:flex flex-col top-[30%] left-0 fixed'>
+    <div className='lg:flex flex-col top-[30%] left-0 fixed'>
 <ul>
     <li className='flex justify-between item-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] duration-300'>
         <a href='https://www.linkedin.com/in/edwardnyaganjeru/' className='flex justify-between items-center w-full text-white'>
